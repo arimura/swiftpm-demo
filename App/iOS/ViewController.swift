d32ed02a5d041741e30c619c2dc40bdebe2c6733
arimura <33759663+arimura@users.noreply.github.com>
2026-01-06 21:23:07 +0900

    add app

---
+++ App/iOS/ViewController.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "SwiftPM Demo"
+        label.font = UIFont.systemFont(ofSize: 32, weight: .bold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Welcome to SwiftPM Demo App"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Tap Me", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(titleLabel)
+        view.addSubview(messageLabel)
+        view.addSubview(actionButton)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            
+            messageLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            messageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            
+            actionButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            actionButton.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 40),
+            actionButton.widthAnchor.constraint(equalToConstant: 200),
+            actionButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        actionButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func buttonTapped() {
+        let alert = UIAlertController(
+            title: "Hello!",
+            message: "Button was tapped",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+}
